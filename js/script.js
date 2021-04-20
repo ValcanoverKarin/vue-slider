9ca6e84d86d9ffa0af00435ea6a1cbf8dad89ba9
karin valcanover <valcanoverkarin@outlook.it>
2021-04-20 16:17:34 +0200

    creato array di immagini

--- js/script.js
+++ js/script.js
@@ -3,6 +3,15 @@ var app = new Vue (
     {
         el: '#root',
         data: {
-        }
+            //1c. l'immagine attiva in quel momento
+            currentActiveImage: 0,
+            //1b. array di immagini
+            sliderImages: [
+                'img/campagna.jpg',
+                'img/cascate.webp',
+                'img/fiori.jpeg',
+                'img/montagne.webp'
+            ],
+        },
     }
 );
