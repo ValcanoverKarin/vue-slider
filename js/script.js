3e643d014c77ff238cb03d2c4cb11f5d93c3c523
karin valcanover <valcanoverkarin@outlook.it>
2021-04-20 17:16:55 +0200

    ho aggiunto il click alle frecce per passare alle immagini sucessive o precedenti

--- js/script.js
+++ js/script.js
@@ -10,8 +10,30 @@ var app = new Vue (
                 'img/campagna.jpg',
                 'img/cascate.webp',
                 'img/fiori.jpeg',
-                'img/montagne.webp'
+                'img/montagne.webp',
             ],
         },
+        methods: {
+            //1g. aggiungo il click della freccia
+            nextPhoto() {
+                //1h. schiacciando sulla freccia passo all 'immagine successiva
+                if ( this.currentActiveImage == this.sliderImages.length - 1 ) {
+                    this.currentActiveImage = 0;
+                } else {
+                    this.currentActiveImage = this.currentActiveImage + 1;
+                }
+            },
+
+            //1j. aggiungo il click della freccia
+            prevPhoto() {
+                //1k. schiacciando sulla freccia passo all 'immagine precedente
+                if ( this.currentActiveImage == 0 ) {
+                    this.currentActiveImage = this.sliderImages.length - 1 ;
+                } else {
+                    this.currentActiveImage = this.currentActiveImage - 1;
+                }
+            }
+            
+        }
     }
 );
